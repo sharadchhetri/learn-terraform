9ce2f24fc19bdc7d9a3524eca0560f8d4703dfb5
Sharad Chhetri <sharadchhetri@gmail.com>
2026-06-09 16:56:05 +0530

    backend

--- variables.tf
+++ variables.tf
@@ -3,5 +3,3 @@ variable "vpc_cidr" {}
 variable "infra_environment" { type = string }
 variable "app_s3_bucket" { type = list(string)}
 variable "s3_bucket_name" { type = string }
-variable "s3_backend_bucket" { type = string }
-variable "backend_key" { type = string}
