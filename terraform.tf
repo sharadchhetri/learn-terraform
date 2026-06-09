01486ed4cd91e13591016528d041e9d4da3f0477
Sharad Chhetri <sharadchhetri@gmail.com>
2026-06-09 16:28:33 +0530

    blank

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "Hashicorp/aws"
+        version = "~> 6.0"
+      }
+      random = {
+        source = "Hashicorp/random"
+      }
+    }
+    
+    backend "s3" {
+      bucket = "xyzcorp-terraform-state"
+      key = "dev/terraform.tfstate"
+      region = "us-east-2"
+      encrypt = true
+      use_lockfile = true
+    }
+}
